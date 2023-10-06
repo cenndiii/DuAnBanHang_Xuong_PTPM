362e0e2226d0c3378d388661a14abfd0388e007e
HellodaylaGiang <giangtrinh107@gmail.com>
2023-10-06 16:38:35 +0700

    hh

--- src/form/BanHangController.java
+++ src/form/BanHangController.java
@@ -4,7 +4,9 @@
  */
 package form;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import javax.swing.JTable;
 
 /**
@@ -15,6 +17,7 @@ public class BanHangController {
 
     ArrayList<SanPhamModel> listSanPham = new ArrayList<>();
     ArrayList<SanPhamModel> spGioHang = new ArrayList<>();
+    ArrayList<HoaDon> listHD = new ArrayList<>();
 
     public BanHangController() {
     }
@@ -27,6 +30,17 @@ public class BanHangController {
         listSanPham.add(new SanPhamModel(5, "SP5", "may hut bui", "2020", 2.0, "nho gon", 15, 150000.0, 200000.0));
 
         return listSanPham;
+    }
+
+    public ArrayList<HoaDon> getHoaDon(int index) {
+        Date now = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//        Object[] obj = new Object[]{"Chờ thanh toán", "Hủy", "Chưa xử lý"};
+        listHD.add(new HoaDon(index, "HD1", sdf.format(now), "Kien", "Chờ thanh toán"));
+//        listHD.add(new HoaDon(index, "HD2", sdf.format(now), "Giang", "Hủy"));
+//        listHD.add(new HoaDon(index, "HD3", sdf.format(now), "Hoan", "Chưa xử lý"));
+
+        return listHD;
     }
 
     public Object[] fillCart(JTable tblProduct, int quantity) {
@@ -43,7 +57,7 @@ public class BanHangController {
         // thêm dữ liệu vào obj để fill vào bảng
         for (SanPhamModel sanPhamModel : spGioHang) {
             obj = new Object[]{sanPhamModel.getIndex(), sanPhamModel.getMaSp(), sanPhamModel.getTenSp(), quantity, sanPhamModel.getGiaBan(), sanPhamModel.getGiaBan() * quantity};
-            
+
             // tính tổng tiền phải trả 
             total += sanPhamModel.getGiaBan() * quantity;
             sanPhamModel.setThanhTien(total);
